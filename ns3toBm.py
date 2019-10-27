b0df4958aef4b42a9cb8bfe138499113237db76b
Oscar Bautista <ogbautista@gmail.com>
2019-10-26 22:23:05 -0400

    added argument to specify scenario simulation time

--- ns3toBm.py
+++ ns3toBm.py
@@ -16,12 +16,23 @@ from my_utils.ns3Time import Ns3Time
 from my_utils.ns3Vector import Ns3Vector
 
 if len(sys.argv) < 3:
-    print ("\nUsage:\npython course2bm.py 'waypointsfile.csv' 'bonnmotionfile.movements' [maxtime=...]\n")
+    print ("\nUsage:\npython ns3toBm.py 'waypointsfile.csv' 'bonnmotionfile.movements' [stime=...]\n")
     sys.exit(1)
 
 sourceFile = sys.argv[1]
 bmScenarioFile = sys.argv[2]
 scn3D = False
+simtime = None
+# Simulation time stime is specified to indicate that the scenario duration extends beyond the last waypoint time
+if len(sys.argv) >= 4:
+    argpair = sys.argv[3].split('=')
+    if len(argpair) == 2 and argpair[0] == "stime":
+        try:
+            simtime = float(argpair[1])
+        except ValueError as e:
+            print(e, "...ignored")
+    else:
+        print("Invalid argument: {} ...ignored".format(sys.argv[3]))
 
 # This dictionary stores the movement information for each node as a list element
 bmMovements = {}
@@ -43,12 +54,9 @@ for waypoint in waypoints:
         bmMovements[node].extend([time, position.x, position.y, position.z])
     else:
         # First Waypoint stored as initial Waypoint
+        bmMovements[node] = [0.0, position.x, position.y, position.z]
         if time > 0:
-            bmMovements[node] = [0.0, position.x, position.y, position.z]
-        try:
             bmMovements[node].extend([time, position.x, position.y, position.z])
-        except KeyError:
-            bmMovements[node] = [time, position.x, position.y, position.z]
 
 # Creates output file and stores the data
 try:
@@ -56,6 +64,11 @@ try:
         if scn3D:
             movementsfile.write("#3D\n")
         for node, data in sorted(bmMovements.items()):
+            # insert one more waypoint if simulation time beyond last waypoint is specifie as argument
+            if simtime is not None and data[-4] < simtime:
+                lastwp = data[-3:]
+                data.append(simtime)
+                data.extend(lastwp)
             line = ""
             for i in range(0, len(data), 4):
                 line += f"{data[i]} {data[i+1]} {data[i+2]} "
